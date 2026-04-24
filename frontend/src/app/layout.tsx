4d63295d9ed9af5e256b28e3f023f2be90bb70df
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-04-24 02:08:12 -0600

    updated with user_table

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+ 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,3 +31,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+
