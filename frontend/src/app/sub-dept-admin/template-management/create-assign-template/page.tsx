8f30988a5e75783a4b7e9a3a7143b1408c40269c
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-05-04 03:14:52 -0600

    Admins Assignment

---
+++ frontend/src/app/sub-dept-admin/template-management/create-assign-template/page.tsx
@@ -0,0 +1,6 @@
+import TemplateCreateBase from "@/components/template_creation_assignment/TemplateCreateBase";
+
+export default function CreateTemplatePage() {
+  // HQ Admin = level 1. Pass the actual role level from your session/auth here.
+  return <TemplateCreateBase level={5} />;
+}
