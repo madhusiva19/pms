f4b43a0fe3d66bc3cfd7f9b9967498b6a4a028be
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-05-05 21:09:36 -0600

    Templates Assignment2

--- frontend/src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-
-export function createClient() {
-  return createBrowserClient(
-    "https://yqdrcdkqwiqtmbyuntwk.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxZHJjZGtxd2lxdG1ieXVudHdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5NDkwMzUsImV4cCI6MjA4NjUyNTAzNX0.l7tCKgIf2wTNPqRcareeOnHATr-XqF-wS68mzQ1gDZQ"
-  );
-}
