e825fd55989edc0cb519ad2d421ec33161adba5c
WathsalaKJ <sandunikekulawala@gmail.com>
2026-05-11 15:17:03 +0530

    create Employee notification page route

---
+++ frontend/src/app/employee/notification/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import NotificationTemplate from '@/components/notifications/NotificationTemplate';
+
+export default function EmployeeNotificationPage() {
+  return <NotificationTemplate />;
+}
