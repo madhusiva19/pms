f6168ec729b54ed9e226f3c1e7b833865e0ed5e9
WathsalaKJ <sandunikekulawala@gmail.com>
2026-05-12 14:56:11 +0530

    frontend colour change in AI recomendations

--- frontend/src/components/AIRecommendationsList.tsx
+++ frontend/src/components/AIRecommendationsList.tsx
@@ -21,8 +21,8 @@ export default function AIRecommendationsList({ recommendations }: AIRecommendat
         <div className="bg-white border border-[#E5E7EB] rounded-xl px-6 pt-6 pb-6">
             {/* Header */}
             <div className="flex items-center gap-2 mb-1.5">
-                <div className="w-5 h-5 flex items-center justify-center bg-[#F3E8FF] rounded-md">
-                    <Sparkles className="w-4 h-4 text-[#9810FA]" />
+                <div className="w-5 h-5 flex items-center justify-center bg-[#EFF6FF] rounded-md">
+                    <Sparkles className="w-4 h-4 text-[#2563EB]" />
                 </div>
                 <h4 className="text-[16px] font-semibold text-[#1E293B] leading-4">
                     AI-Powered Recommendations
@@ -37,7 +37,8 @@ export default function AIRecommendationsList({ recommendations }: AIRecommendat
             <div className="flex flex-col gap-3">
                 {recommendations.map((item, index) => (
                     <div key={index} className="flex items-start gap-3">
-                        <div className="mt-[9px] w-1.5 h-1.5 rounded-full bg-[#AD46FF] flex-shrink-0" />
+                        {/* dots*/}
+                        <div className="mt-[9px] w-1.5 h-1.5 rounded-full bg-[#2563EB] flex-shrink-0" />
                         <p className="text-[15px] text-[#364153] leading-6">{item.text}</p>
                     </div>
                 ))}
