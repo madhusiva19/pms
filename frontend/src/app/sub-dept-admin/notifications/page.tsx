a93014af19c1cba58aa30b8168390e0016030eb2
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-05-15 05:23:30 -0600

    Assignment Page

--- frontend/src/app/sub-dept-admin/notifications/page.tsx
+++ frontend/src/app/sub-dept-admin/notifications/page.tsx
@@ -1,5 +1,5 @@
 // app/(country-admin)/notifications/page.tsx
  import NotificationsPage from "@/components/notifications/NotificationsPage";
  export default function Page() {
-  return <NotificationsPage level={2} />;
+  return <NotificationsPage level={5} />;
  }
