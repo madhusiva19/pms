6e78b9499d99a8e2f55da0abd7940564554c58d8
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-06-02 04:50:56 -0600

    Before Merge

--- frontend/src/app/hq-admin/notifications/page.tsx
+++
@@ -1,5 +0,0 @@
-// app/(hq-admin)/notifications/page.tsx
-import NotificationsPage from "@/components/notifications/NotificationsPage";
-export default function Page() {
-  return <NotificationsPage level={1} />;
-}
