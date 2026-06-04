0c5f95f048f2448be5a266029d43e29a03534c4a
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-06-04 12:39:28 -0600

    merged12

--- frontend/src/app/country-admin/template-management/page.tsx
+++
@@ -1,5 +0,0 @@
-// app/(country-admin)/templates/page.tsx
-import TemplateDashboardBase from "@/components/templatedashboard/TemplateDashboardBase";
-export default function CountryAdminTemplatePage() {
-  return <TemplateDashboardBase level={2} />;
-}
--- frontend/src/app/sub-dept-admin/notifications/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import NotificationsPage from "@/components/notifications/NotificationsPage";
-
-export default function SubDeptAdminNotificationsPage() {
-  return <NotificationsPage level={5} />;
-}
