963398aba072993f8678c2c8a9578eb974a53275
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-06-04 12:39:28 -0600

    merged12

--- frontend/src/app/dept-admin/template-management/page.tsx
+++
@@ -1,5 +0,0 @@
-// app/(dept-admin)/templates/page.tsx
-import TemplateDashboardBase from "@/components/templatedashboard/TemplateDashboardBase";
-export default function DeptAdminTemplatePage() {
-  return <TemplateDashboardBase level={4} />;
-}
--- frontend/src/app/sub-dept-admin/template-management/page.tsx
+++
@@ -1,6 +0,0 @@
-import TemplateDashboardBase from "@/components/templatedashboard/TemplateDashboardBase";
-
-
-export default function SubDeptAdminTemplatePage() {
-  return <TemplateDashboardBase level={5} />;
-}
