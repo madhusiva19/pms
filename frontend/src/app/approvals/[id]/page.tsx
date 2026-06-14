bccdbd69ae2189355c4fcc38f4a0fa83bee0c011
Nethmini Rathnayake <smnethminirathnayake2003@gmail.com>
2026-06-13 23:03:26 -0600

    fixed errors with dev-final

--- frontend/src/app/approvals/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ApprovalDetail from './_components/ApprovalDetailView';
-import ClientRoute from '../../ClientRoute';
-
-export default function ApprovalDetailRoute() {
-  return <ClientRoute><ApprovalDetail /></ClientRoute>;
-}
